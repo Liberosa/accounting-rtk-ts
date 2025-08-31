ca0bb2921da361e17caa402d9d070a85a3d0bc81
Liberosa <40002607+Liberosa@users.noreply.github.com>
2025-08-31 18:44:48 +0300

    Persist Redux state to localStorage and restore on initialization
    - Configured `store` to preload state from `localStorage`.
    - Subscribed `store` changes to sync state with `localStorage`.
    - Updated `store` type definitions with `UserProfile`.

--- app/store.ts
+++ app/store.ts
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import user from "../features/user/userSlice";
 import token from "../features/token/tokenSlice";
+import type {UserProfile} from "../utils/types";
+
 export const store = configureStore({
     reducer: {
-        token,user
+        token, user
     },
+    preloadedState: JSON.parse(localStorage.getItem('state') || '{}') as { token: string, user: UserProfile}
 })
+store.subscribe(() => localStorage.setItem('state', JSON.stringify(store.getState())));
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
